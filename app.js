28067a00b91c8c2b2c59c0934b44a925d30982b5
Андрей Деревянкин <a_derevyankin@MacBook-Air-Andrej.local>
2023-07-25 17:06:43 +0300

    redis starter подключен в виде пакета вместо файла в проекте

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import redisStarter from './redis-starter.js'
+import redisStarter from '@yababay67/redis-starter'
 
 dotenv.config()
 /**
@@ -12,21 +12,20 @@ app.use(redisStarter)
 app.get('/', async (req, res) => {
     const r = Math.random()
     await req.redisClient.set('random', `${r}`)
-    res.end(`${r}`)
-    // res.end(`
-    //     <html>
-    //     <head></head>
-    //     <body>
-    //     <p>Please, check new random value with console command:</p>
-    //     <pre>
-    //         <code>redis-cli get random</code>
-    //     </pre>
-    //     <p>It should be ${r}.</p>
-    //     <p>You can <button onclick="window.location.reload()">reload</button> the page for one more checking.</p>
-    //     </body>
-    //     </html>
-    // `)
-
+    // res.end(`${r}`)
+    res.end(`
+        <html>
+        <head></head>
+        <body>
+        <p>Please, check new random value with console command:</p>
+        <pre>
+            <code>redis-cli get random</code>
+        </pre>
+        <p>It should be ${r}.</p>
+        <p>You can <button onclick="window.location.reload()">reload</button> the page for one more checking.</p>
+        </body>
+        </html>
+    `)
 })
 
 const httpPort = process.env.HTTP_PORT || 3000
