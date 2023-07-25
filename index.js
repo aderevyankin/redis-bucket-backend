8d0340a1177f902ac3cb09552f165782d8bd8995
mabel <bigbelk@live.ru>
2023-07-25 14:09:02 +0300

    debug

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if(!REDIS_SERVER_PATH) throw 'Please setup path of redis server'
 const prepareDump = async () => {
     if(await exists()){
         const dump = await download()
-        fs.writeFileSync(dump)
+        if(dump) fs.writeFileSync(dump)
     }
 }
 
