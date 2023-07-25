ad757daabaa5210f2d8898159df11da2d19d3012
mabel <bigbelk@live.ru>
2023-07-25 11:33:18 +0300

    basic

--- README.md
+++
@@ -1,9 +0,0 @@
-# redis-bucket-backend
-backend for many projects using Redis and  YC bucket dump
-
-
-Development progress and instructions [(RU)](https://precious-harrier-731.notion.site/AWS-YC-bucket-d60a94c58579413e82b2c4f6560af4ce?pvs=4)
-
-
-TODO:
-https://redis.io/docs/clients/om-clients/stack-node/
---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import redisStarter from '@yababay67/redis-starter'
+
+dotenv.config()
+
+const app = express()
+app.use(redisStarter)
+
+app.get('/', async (req, res) => {
+    const r = Math.random()
+    await req.redisClient.set('random', `${r}`)
+    res.end(```
+        <html>
+        <head></head>
+        <body>
+        <p>Please, check new random value with console command:</p>
+        <pre>
+            <code>redis-cli get random</code>
+        </pre>
+        <p>It should be ${r}.</p>
+        <p>You can reload the page (F5) for one more checking.</p>
+        </body>
+        </html>
+    ```)
+
+})
+
+const httpPort = process.env.HTTP_PORT || 3000
+
+app.listen(HTTP_PORT, () => console.log(`Server is listening on port ${httpPort}`))
