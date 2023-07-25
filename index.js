5715cf1dbb95c3575ca7e41ef83dab132b8f2e37
Андрей Деревянкин <a_derevyankin@MacBook-Air-Andrej.local>
2023-07-25 17:45:47 +0300

    logs&comments

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ const { spawn } = require('child_process')
 const redis = require('redis')
 const md5 = require('md5')
 const { download, upload, exists } = require('@yababay67/s3-uploader')
+/**
+ * Получаем ключи, требующиеся для работы с AWS и redis 
+ */
+const { REDIS_SERVER_PATH, AWS_DEFAULT_KEY, BACKUP_INTERVAL } = process.env
 
-const { REDIS_SERVER_PATH, AWS_DEFAULT_KEY, BUCKUP_INTERVAL } = process.env
-
-if(!REDIS_SERVER_PATH) throw 'Please setup path of redis server'
-
+if (!REDIS_SERVER_PATH) throw 'Please setup path of redis server'
+/**
+ * Получение дампа из облачного хранилища
+ */
 const prepareDump = async () => {
-    if(await exists()){
+    if (await exists()) {
         console.log('Downloading last dump from bucket...')
         const dump = await download()
+        console.log('Downloaded')
         fs.writeFileSync(AWS_DEFAULT_KEY, dump)
+        console.log('Dump saved locally')
     }
 }
 
 let client = null
 let saveInterval = null
 let md5Previous = null
-
+/**
+ * Запуск Redis server 
+ */
 const startRedisServer = () => new Promise((resolve, reject) => {
     const redisServer = spawn(REDIS_SERVER_PATH);
     let message = '';
@@ -27,7 +35,8 @@ const startRedisServer = () => new Promise((resolve, reject) => {
         message += data.toString();
         if (message.includes('Ready to accept connections')) {
             client = redis.createClient()
-            resolve('Redis Server успешно запущен!');
+            console.log('Redis Server successfully ran!');//,
+            resolve();
         }
     });
 
@@ -38,20 +47,24 @@ const startRedisServer = () => new Promise((resolve, reject) => {
 });
 
 module.exports = async (req, _, next) => {
-    if(!client){
+    if (!client) {
         await prepareDump()
         await startRedisServer()
         await client.connect()
+
         saveInterval = setInterval(async () => {
-            if(!fs.existsSync(AWS_DEFAULT_KEY)) return
+            if (!fs.existsSync(AWS_DEFAULT_KEY)) return
             await client.save()
+            console.log('Dump saved locally');
             const md5Current = md5(fs.readFileSync(AWS_DEFAULT_KEY))
             if (md5Current === md5Previous) return
             const stream = fs.createReadStream(AWS_DEFAULT_KEY)
             await upload(stream)
+            console.log('Dump pushed to remote storage');
             md5Previous = md5Current
             console.log(`Backup is uploaded at ${new Date}`)
-        }, +BUCKUP_INTERVAL)
+        }, +BACKUP_INTERVAL)
+
         console.log('Redis server and client are ready.')
     }
     req.redisClient = client
